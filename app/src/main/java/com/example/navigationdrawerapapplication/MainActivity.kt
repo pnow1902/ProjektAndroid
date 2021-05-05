a1300b18e0afac30ab5d28ded1d45db5ad862bd4
pnow1902 <piotrnowinski13@gmail.com>
2021-05-05 18:22:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigationdrawerapapplication/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.navigationdrawerapapplication
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity: AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+    private lateinit var fragmentTransaction: FragmentTransaction
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        this.setTitle(R.string.app_name1)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, 0, 0)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+
+        val fragmentManager = supportFragmentManager
+        fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.container_fragment, Home())
+        fragmentTransaction.commit()
+
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Home())
+                fragmentTransaction.commit()
+            }
+            R.id.nav_team -> {
+                Toast.makeText(this, "Team clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Team())
+                fragmentTransaction.commit()
+            }
+            R.id.nav_reserves -> {
+                Toast.makeText(this, "Reserves clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Reserves())
+                fragmentTransaction.commit()
+            }
+            R.id.nav_staff -> {
+                Toast.makeText(this, "Staff clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Staff())
+                fragmentTransaction.commit()
+            }
+            R.id.nav_training -> {
+                Toast.makeText(this, "Training clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Training())
+                fragmentTransaction.commit()
+            }
+            R.id.nav_contact -> {
+                Toast.makeText(this, "Contact clicked", Toast.LENGTH_SHORT).show()
+                val fragmentManager = supportFragmentManager
+                fragmentTransaction = fragmentManager.beginTransaction()
+                fragmentTransaction.replace(R.id.container_fragment, Contact())
+                fragmentTransaction.commit()
+            }
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
