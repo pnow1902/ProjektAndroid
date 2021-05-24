b60d495abfcca0346519c1bdb1918723a3f93f72
pnow1902 <piotrnowinski13@gmail.com>
2021-05-24 23:29:11 +0200

    Eigth commit (code cleanup)

--- app/src/main/java/com/example/navigationdrawerapapplication/Contact.kt
+++ app/src/main/java/com/example/navigationdrawerapapplication/Contact.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class Contact : Fragment(), View.OnClickListener {
